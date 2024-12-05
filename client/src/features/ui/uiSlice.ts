b25fbaf2c5ab6959764e9184db52d3886ac14e2f
Ilja Novak <birukov0608@gmail.com>
2024-12-05 22:56:16 +0100

    theme switch

---
+++ client/src/features/ui/uiSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+interface UIState {
+  theme: string;
+  isSidebarOpen: boolean;
+}
+
+const initialState: UIState = {
+  theme: "light",
+  isSidebarOpen: false,
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    setTheme: (state, action) => {
+      state.theme = action.payload;
+    },
+    toggleSidebar: (state) => {
+      state.isSidebarOpen = !state.isSidebarOpen;
+    },
+  },
+});
+
+export const { setTheme, toggleSidebar } = uiSlice.actions;
+
+export default uiSlice.reducer;
+
+export const selectTheme = (state: RootState) => state.ui.theme;
+export const selectIsSidebarOpen = (state: RootState) => state.ui.isSidebarOpen;
--- client/src/widgets/Header.tsx
+++ client/src/widgets/Header.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { AppDispatch } from "../app/store";
+import { selectTheme, setTheme } from "../features/ui/uiSlice";
 import Logo from "../shared/ui/Logo";
 import Nav from "../shared/ui/Nav";
+import Button from "../shared/ui/Button";
+import { themes } from "../shared/constants/themes";
 
 function Header() {
+  const dispatch: AppDispatch = useDispatch();
+  const theme = useSelector(selectTheme);
+
+  const toggleTheme = () => {
+    const currentIndex = themes.indexOf(theme);
+    const nextIndex = (currentIndex + 1) % themes.length;
+    dispatch(setTheme(themes[nextIndex]));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <header className="header">
       <div className="container">
@@ -11,11 +31,14 @@ function Header() {
         </Link>
         <Nav />
         <div className="cta">
-          <Link to="/login" className="btn">
-            Login
+          <Button onClick={toggleTheme} variant="primary">
+            Toggle Theme
+          </Button>
+          <Link to="/login">
+            <Button>Login</Button>
           </Link>
-          <Link to="/signup" className="btn btn--primary">
-            Sign Up
+          <Link to="/signup">
+            <Button>Sign Up</Button>
           </Link>
         </div>
       </div>
