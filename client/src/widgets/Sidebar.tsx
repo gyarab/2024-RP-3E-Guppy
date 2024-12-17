1cb732e97655e1c5b2418703a0d01eb29b002c58
Jakub Hanslík <jakub@hanslikovi.com>
2024-12-17 15:47:00 +0100

    Sidebar init

---
+++ client/src/widgets/Sidebar.tsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <div className="organizations">
+
+            </div>
+            <div className="links">
+
+            </div>
+        </aside>
+    );
+}
+
+export default Sidebar;
