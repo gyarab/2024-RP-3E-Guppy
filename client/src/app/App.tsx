4f5b7d3089e4f25b77c016428c77a4fa9d2f666f
Jakub Hanslík <jakub@hanslikovi.com>
2024-12-17 18:44:57 +0100

    Fak ju

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,11 +1,13 @@
 import Header from "../widgets/Header";
 import Footer from "../widgets/Footer";
 import Router from "./routes/Router";
+import Sidebar from "../widgets/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
+      <Sidebar />
       <Router />
       <Footer />
     </>
