cd9ad96507991f35630cfdadd648dab41fe46d81
Ilja Novak <birukov0608@gmail.com>
2024-12-28 12:13:30 +0100

    serve static uploads directory

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { TokenModule } from './modules/token/token.module';
@@ -8,11 +9,16 @@ import { PostModule } from './modules/post/post.module';
 import { CommentModule } from './modules/comment/comment.module';
 import { OrganizationModule } from './modules/organization/organization.module';
 import { UploadModule } from './modules/upload/upload.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
     }),
     PrismaModule,
     TokenModule,
