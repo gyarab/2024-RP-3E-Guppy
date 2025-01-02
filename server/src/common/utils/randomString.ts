1274710790e4ed1ee1c876836dc14e4198e6bf78
Ilja Novak <birukov0608@gmail.com>
2025-01-02 17:04:04 +0100

    jiny algorithmus vytvareni random retezce (+ moznost nastavit z jakych znaku bude)

--- server/src/common/utils/randomString.ts
+++ server/src/common/utils/randomString.ts
@@ -1,5 +1,9 @@
 export async function generateRandomString(length: number): Promise<string> {
-  return String.fromCharCode(
-    ...Array.from({ length }, () => Math.floor(Math.random() * 26) + 97),
-  );
+  // nektery znaky jsou vynechany, aby se treba nepletly 0 a O nebo 1 a I apod.
+  const characters = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjikmnpqrstuvwxyz2346789';
+  let code = '';
+  for (let i = 0; i < length; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
 }
