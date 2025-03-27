e947d116637abe1be76484182e4f63f4a007a355
Ilja Novak <birukov0608@gmail.com>
2025-03-27 10:04:33 +0100

    hot fix

--- client/src/shared/interfaces/Organization.ts
+++ client/src/shared/interfaces/Organization.ts
@@ -1,24 +1,13 @@
 import { Post } from "./Post";
 import { User } from "./User";
 
-interface UserOrganization {
-  id: number;
-  organizationId: number;
-  role: { name: string };
-  roleId: number;
-  user: User;
-  userId: number;
-}
-
 export interface Organization {
   id: number;
   name: string;
   description: string;
-  // joinCode: string;
   logoUrl: string;
   posts?: Post[];
   createdAt: Date;
-  // users: UserOrganization[];
 }
 
 export interface OrgnizationWithJoin extends Organization {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,16 +21,4 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
       },
-<<<<<<< HEAD
-=======
-      '/uploads': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    host: "0.0.0.0",
-    port: 5173,
-  },
->>>>>>> main
 });
